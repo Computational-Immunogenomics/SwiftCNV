4223e7809cd3b24363091036dd658b7bfb5fa047
AdrianParrilla <parrilladrian@gmail.com>
2026-07-28 16:40:43 +0200

    restructrured, created resources folder

--- src/swiftcnv/datasets/__init__.py
+++ src/swiftcnv/datasets/__init__.py
@@ -7,6 +7,6 @@ def Qian2020_Ovarian() -> AnnData:
 	'''
 	Derived from :cite:`https://www.weizmann.ac.il/sites/3CA/ovarian`.
 	'''
-	resource = files('swiftcnv.data').joinpath('datasets', 'Qian2020_Ovarian.h5ad')
+	resource = files('swiftcnv.datasets').joinpath('Qian2020_Ovarian.h5ad')
 	with as_file(resource) as filepath:
 		return sc.read_h5ad(filepath)
